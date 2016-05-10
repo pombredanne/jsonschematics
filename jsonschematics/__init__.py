a8dd2989f1b35ab95c8d7516b9e608964182b900
Bob Lannon <blannon@picwell.com>
2016-05-10 13:38:20 -0400

    other updates

--- jsonschematics/__init__.py
+++ jsonschematics/__init__.py
@@ -10,12 +10,12 @@ __version__ = '1.0'
 
 
 SCHEMATIC_TYPE_TO_JSON_TYPE = {
-    NumberType: 'number',
-    IntType: 'integer',
-    LongType: 'integer',
-    FloatType: 'number',
-    DecimalType: 'number',
-    BooleanType: 'boolean',
+    'NumberType': 'number',
+    'IntType': 'integer',
+    'LongType': 'integer',
+    'FloatType': 'number',
+    'DecimalType': 'number',
+    'BooleanType': 'boolean',
 }
 
 # Schema Serialization
@@ -27,35 +27,59 @@ schema_kwargs_to_schematics = {
     'pattern': 'regex',
     'minimum': 'min_value',
     'maximum': 'max_value',
-    'enum': 'choices',
-    'default': 'default'
+    'enum': 'choices'
 }
+
+
+def jsonschema_for_single_field(field_instance):
+    field_schema = {}
+
+    field_schema["type"] = SCHEMATIC_TYPE_TO_JSON_TYPE.get(field_instance.__class__.__name__, 'string')
+
+    field_schema["title"] = field_instance.metadata.get('label', '')
+    field_schema["description"] = field_instance.metadata.get('description', '')
+
+    for js_key, schematic_key in iteritems(schema_kwargs_to_schematics):
+        value = getattr(field_instance, schematic_key, None)
+        if value is not None:
+            field_schema[js_key] = value
+
+    return field_schema
 
 
 def jsonschema_for_fields(model):
     properties = {}
     required = []
-    for field_name, field_instance in iteritems(model._fields):
+    for field_name, field_instance in iteritems(model.fields):
         serialized_name = getattr(field_instance, 'serialized_name', None) or field_name
 
         if isinstance(field_instance, ModelType):
-            properties[serialized_name] = jsonschema_for_model(field_instance.model_class)
+            node = jsonschema_for_model(field_instance.model_class)
 
         elif isinstance(field_instance, ListType):
-            properties[serialized_name] = jsonschema_for_model(field_instance.model_class, 'array')
+            try:
+                node = jsonschema_for_model(field_instance.model_class, 'array')
+            except AttributeError:
+                field_schema = jsonschema_for_single_field(field_instance.field)
+                node = {
+                    'type': 'array',
+                    'items': field_schema
+                }
 
         # Convert field as single model
         elif isinstance(field_instance, BaseType):
-            properties[serialized_name] = {
-                "type": SCHEMATIC_TYPE_TO_JSON_TYPE.get(field_instance.__class__, 'string')
-            }
-            for js_key, schematic_key in iteritems(schema_kwargs_to_schematics):
-                value = getattr(field_instance, schematic_key, None)
-                if value is not None:
-                    properties[serialized_name][js_key] = value
+            node = jsonschema_for_single_field(field_instance)
 
         if getattr(field_instance, 'required', False):
             required.append(serialized_name)
+            properties[serialized_name] = node
+        else:
+            properties[serialized_name] = {
+                "oneOf":  [
+                    {'type': 'null'},
+                    node
+                ]
+            }
 
     return properties, required
 
@@ -66,8 +90,8 @@ def jsonschema_for_model(model, _type='object'):
 
     schema = {
         'type': 'object',
-        'title': model.__name__,
-        'description': model.__doc__,
+        'title': model.metadata.get("label", ""),
+        'description': model.metadata.get("description", ""),
         'properties': properties,
     }
 
@@ -77,7 +101,7 @@ def jsonschema_for_model(model, _type='object'):
     if _type == 'array':
         schema = {
             'type': 'array',
-            'title': '%s Set' % (model.__name__),
+            #'title': '%s Set' % (model.__name__),
             'items': schema,
         }
 
@@ -86,4 +110,9 @@ def jsonschema_for_model(model, _type='object'):
 
 def to_jsonschema(model, **kwargs):
     """Returns a representation of this schema class as a JSON schema."""
-    return json.dumps(jsonschema_for_model(model), **kwargs)
+    jsonschema = jsonschema_for_model(model)
+    jsonschema['$schema'] = 'http://json-schema.org/draft-04/schema#'
+    schema_id = kwargs.pop('schema_id', None)
+    if schema_id is not None:
+        jsonschema['id'] = schema_id
+    return json.dumps(jsonschema, **kwargs)
